74dcdcf330a4f19465bc94e83cadbeef364afbc1
Colin Suckow <csuckow99@gmail.com>
2019-01-27 20:45:19 -0800

    Finished memory tests

--- chip8emuTests/chip8emuTests/MemoryTest.cs
+++ chip8emuTests/chip8emuTests/MemoryTest.cs
@@ -20,13 +20,38 @@ namespace chip8emuTests
             ushort memLocation2 = memory.ReadByte(4095);
 
             Assert.AreEqual(writeValue, memLocation1, 0, "Failed lower memory bound check");
-            Assert.AreEqual(writeValue, memLocation2, 0, "Failed upper memory bound check");
-            
+            Assert.AreEqual(writeValue, memLocation2, 0, "Failed upper memory bound check");  
         }
 
         [TestMethod]
         public void Register_Write_And_Read()
         {
+            Memory memory = new Memory();
+            ushort writeValue = 10;
+
+            memory.registers["V0"] = writeValue;
+
+            Assert.AreEqual(writeValue, memory.registers["V0"]);
+        }
+
+        [TestMethod]
+        public void Delay_And_Sound_Update()
+        {
+            Memory memory = new Memory();
+
+            //Cycle the timers once and check to make sure they dont deincrement when their value is 0
+            memory.UpdateTimers(); 
+            Assert.AreEqual(0, memory.registers["Delay"], 0, "Deincremented when 0");
+            Assert.AreEqual(0, memory.registers["Sound"], 0, "Deincremented when 0");
+
+            ushort writeValue = 10;
+
+            memory.registers["Delay"] = writeValue;
+            memory.registers["Sound"] = writeValue;
+            memory.UpdateTimers();
+
+            Assert.AreEqual(writeValue - 1, memory.registers["Delay"], 0, "Did not deincrement delay properly");
+            Assert.AreEqual(writeValue -1, memory.registers["Sound"], 0, "Did not deincrement sound properly");
 
         }
     }
